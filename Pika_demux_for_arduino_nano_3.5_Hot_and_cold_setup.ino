48390a35f8b615237f8734ff638a8d4332d6c9f3
minusmagis <33642118+minusmagis@users.noreply.github.com>
2020-09-07 17:19:37 +0200

    Update Pika_demux_for_arduino_nano_3.5_Hot_and_cold_setup.ino

--- Pika_demux_for_arduino_nano_3.5_Hot_and_cold_setup.ino
+++ Pika_demux_for_arduino_nano_3.5_Hot_and_cold_setup.ino
@@ -9,55 +9,40 @@
 
                                                                           READ THE LINE IMMEDIATELY ABOVE
 
-    Old mapping: to new mapping (of the green pcb and on the lower pcb side
-
-    L     R
-
-    L.06  L.10
-    L.05  L.09
-    L.08  L.12
-    L.07  L.11
-    R.10  L.03
-    R.09  L.04
-    R.12  L.01
-    R.11  L.02
-    R.06  R.03
-    R.05  R.04
-    R.08  R.01
-    R.07  R.02
+    2020 09 07 changes: The mapping was completely wrong and we are now recalibrating it
 
 */
 
 
 
-String Cell; //String that will determine which cell is being measured
-String CellLabel; //String that will be the name of the cell
-String CellBit; //The bitcode of each cell
-uint8_t Bit0 = 0; // Ground Bit, it will always be zero
-uint8_t Bit1 = 0; //Bits that will determine the signal emitted to the arduino, hence the cell that will be connected
+String Cell;                          //String that will determine which cell is being measured
+String CellLabel;                     //String that will be the name of the cell
+String CellBit;                       //The bitcode of each cell
+uint8_t Bit0 = 0;                     // Ground Bit, it will always be zero
+uint8_t Bit1 = 0;                     //Bits that will determine the signal emitted to the arduino, hence the cell that will be connected
 uint8_t Bit2 = 0;
 uint8_t Bit3 = 0;
 uint8_t Bit4 = 0;
 uint8_t Bit5 = 0;
-uint8_t NotBit1 = 0; // stores the values of the reset pins
+uint8_t NotBit1 = 0;                  // Stores the values of the reset pins
 uint8_t NotBit2 = 0;
 uint8_t NotBit3 = 0;
 uint8_t NotBit4 = 0;
 uint8_t NotBit5 = 0;
 
 
-#define PWMPin 9
-#define HnCPin 10         // hot is Low and cold is high
+#define PWMPin 9                      // Pin that will be used to PWM the signal
+#define HnCPin 10                     // Pin that will decide the polarity of the H bridge (hot is Low and cold is high)
 
 
+// This is a very simple setup loop that sets all pins as outputs and begins serial coms
 void setup()
 {
-  //  Serial.println("Multiplexer and HnC arduino");
-  for (uint8_t pin = 2; pin < 14; pin++)
+  for (uint8_t pin = 2; pin < 14; pin++) //First we define all pins as outputs
   {
-    pinMode(pin, OUTPUT);             //Defines pins from 2 to 13 as an output
+    pinMode(pin, OUTPUT);             // Defines pins from 2 to 13 as an output
   }
-  pinMode(A0, OUTPUT);
+  pinMode(A0, OUTPUT);                // Also the analog pins
   pinMode(A1, OUTPUT);
   pinMode(A2, OUTPUT);
   pinMode(A3, OUTPUT);
@@ -66,40 +51,50 @@ void setup()
   pinMode(A6, OUTPUT);
   pinMode(A7, OUTPUT);
 
-  Serial.begin(9600);                 // Begins Serial communication
-
+  Serial.begin(9600);                 // We begin Serial communication
+  Serial.println("Arduino Multiplexer Hot N' Cold");        // We also print a welcome message so that we know which arduino we are talking to
 }
+
+// The main loop waits for an input within the serial connection and when provided with one evaluates if it fits within the available commands.
+// Whenever feeded a valid comand the loop executes the corresponding function. This programming style is a bit clumsy since I programmed it with little knowledge about C++
 
 void loop()
 {
-  if (Serial.available() > 0)    // If the serial is trying to communicate calls the functiond SelectedCell() stated below and
-  {
-    //    Serial.println("-----------------------------------------------");  // print a dashed line
-    Cell = SelectedCell();                // assigns its value to the string Cell that will determine which cell is being measured
-    //Serial.println(Cell);
-    if (Cell.length() > 3)
-    {
-      // Calls the function of Selected CellName that will output the label of the cell as well as the bitstring needed to contact that specific cell
-      //      Serial.print("Plain data from serial = ");
-      CellBit = SelectedCellName(Cell);      // Assign the binary code of the cell being read to the variable CellBit
-      //Serial.println(CellBit);                // Prints the cell that is being read
-      if (CellBit != "")                    // If there is data in the CellBit
+  if (Serial.available() > 0)                                                 // If the serial is trying to communicate calls the functiond SelectedCell() stated below and
+  {                                                                           // assigns its value to the string Cell that will determine which cell is being measured
+    Cell = SelectedCell();                                                    // The function SelectedCell extracts the characters from the serial and stores them within a string                               
+    
+    //Serial.println(Cell);                                                   // For debugging purposes
+    
+    if (Cell.length() > 3)                                                    // If the length of the command is longer than 3, meaning it is most likely a complete command c
+    {                                                                         // we call the function SelectedCellName that will output the label of the cell as well as the bitstring needed to contact that specific cell
+          
+      //Serial.print("Plain data from serial = ");                            // For debugging purposes
+      
+      CellBit = SelectedCellName(Cell);                                       // We assign the bitstring (binary code) of the cell that we desire to connect to the variable CellBit
+      
+      //Serial.println(CellBit);                                              // For debugging purposes
+      
+      if (CellBit != "")                                                      // If the bitstring is not empty, it means we have a valid input that we can use to decide which relays we have to flip
       {
-        //        Serial.print("     Cell binary code = ");  // Print "        Cell binary code ="
-        //        Serial.println(CellBit);                   // Print the binary code associated with the cell being read
-        //        Serial.println("-----------------------------------------------");  // print a dashed line
-        Bit1 = ReadBit(CellBit, 1);  //Assign the value of the first bit from the bitcode to a variable Bit1
-        Bit2 = ReadBit(CellBit, 2);  //Assign the value of the second bit from the bitcode to a variable Bit2
-        Bit3 = ReadBit(CellBit, 3);  //Assign the value of the third bit from the bitcode to a variable Bit3
-        Bit4 = ReadBit(CellBit, 4);  //Assign the value of the forth bit from the bitcode to a variable Bit4
-        Bit5 = ReadBit(CellBit, 5);  //Assign the value of the fifth bit from the bitcode to a variable Bit5
-        NotBit1 = invert(Bit1);      //Use the function invert to generate the complementary values from BitX values
+        
+        //        Serial.print("     Cell binary code = ");                   // For debugging purposes
+        //        Serial.println(CellBit);                   
+        //        Serial.println("-----------------------------------------------"); 
+        
+        Bit1 = ReadBit(CellBit, 1);                                           // Assign the value of the first bit from the bitcode to a variable Bit1
+        Bit2 = ReadBit(CellBit, 2);                                           // Assign the value of the second bit from the bitcode to a variable Bit2
+        Bit3 = ReadBit(CellBit, 3);                                           // Assign the value of the third bit from the bitcode to a variable Bit3
+        Bit4 = ReadBit(CellBit, 4);                                           // Assign the value of the forth bit from the bitcode to a variable Bit4
+        Bit5 = ReadBit(CellBit, 5);                                           // Assign the value of the fifth bit from the bitcode to a variable Bit5
+        NotBit1 = invert(Bit1);                                               // Use the function invert to generate the complementary values from BitX values
         NotBit2 = invert(Bit2);
         NotBit3 = invert(Bit3);
         NotBit4 = invert(Bit4);
         NotBit5 = invert(Bit5);
-        //        Serial.print(Bit1);       //Troubleshooting to see if BitX values are actually a binary value and to see if the complementary are really complementary
-        //        Serial.println(NotBit1);  // If it is a zero on the right it should be a one on the left
+        
+        //        Serial.print(Bit1);                                         // For debugging purposes
+        //        Serial.println(NotBit1);  
         //        Serial.print(Bit2);
         //        Serial.println(NotBit2);
         //        Serial.print(Bit3);
@@ -109,15 +104,21 @@ void loop()
         //        Serial.print(Bit5);
         //        Serial.println(NotBit5);
         //        Serial.println("-----------------------------------------------");
-        //        Serial.println("Please introduce the next cell label you want to measure:");  //Write this message on the serial port so that the use knows that he can change the cell number
-        digitalWrite(2, LOW);        //The pin 0 reset and set is always ground in this case on corresponds to pin 14 and the other to pin 2 on the arduino, pin 14 is ground by default in arduino
-        digitalWrite(3, Bit1);     //Write the value of BitX (etither 0 or 1) to each pin on the arduino to contact the cell
-        digitalWrite(2, NotBit1);
-        delay(5);
-        digitalWrite(2, LOW);
+        //        Serial.println("Please introduce the next cell label you want to measure:");  // Write this message on the serial port so that the use knows that he can change the cell number
+
+        digitalWrite(2, LOW);                                                 // The pin 0 reset and set is always ground in this case one corresponds to pin 14 and the other to pin 2 on the arduino,
+                                                                              // pin 14 is ground by default in arduino
+                                                                              
+        digitalWrite(3, Bit1);                                                // Write the value of BitX (etither 0 or 1) to each pin on the arduino to contact the cell
+        digitalWrite(2, NotBit1);                                             // We write the inverse value on the reset pins so that we make sure each relay is on the desired position. (These relays are bistable so they need to be set
+                                                                              // and reset so that they are placed on the propper position
+        delay(5);                                                             // We introduce a small delay so that the relays have time to switch
+                                                                                  
+        digitalWrite(2, LOW);                                                 // We set the set and reset to low to lower power consumption
         digitalWrite(3, LOW);
         delay(5);
-        digitalWrite(A4, Bit2);
+        
+        digitalWrite(A4, Bit2);                                               // We repeat for every pin untill every relay is on their desired position
         digitalWrite(A5, NotBit2);
         delay(5);
         digitalWrite(A4, LOW);
@@ -208,101 +209,101 @@ String SelectedCellName(String CellName)
   String HotCold = String("");
   HotCold = CellName.substring(0, 2);
 
-  if (CellName == "82464849")                           //R.01
+  if (CellName == "82464849")                               //R.01 *
   {
-    bitString = String("10110");                            //R.01
+    bitString = String("01000");                            //R.01 *
   }
-  else if (CellName == "82464850")                          //R.02
+  else if (CellName == "82464850")                          //R.02 *
   {
-    bitString = String("10111");                            //R.02
+    bitString = String("01001");                            //R.02 *
   }
-  else if (CellName == "82464851")                          //R.03
+  else if (CellName == "82464851")                          //R.03 *
   {
-    bitString = String("10101");                            //R.03
+    bitString = String("01011");                            //R.03 *
   }
-  else if (CellName == "82464852")                          //R.04
+  else if (CellName == "82464852")                          //R.04 *
   {
-    bitString = String("10100");                            //R.04
+    bitString = String("01010");                            //R.04 *
   }
-  else if (CellName == "82464853")                          //R.05
+  else if (CellName == "82464853")                          //R.05 *
   {
-    bitString = String("01000");                            //R.05
+    bitString = String("11100");                            //R.05 *
   }
-  else if (CellName == "82464854")                          //R.06
+  else if (CellName == "82464854")                          //R.06 *
   {
-    bitString = String("01001");                            //R.06
+    bitString = String("11101");                            //R.06 *
   }
-  else if (CellName == "82464855")                          //R.07
+  else if (CellName == "82464855")                          //R.07 *
   {
-    bitString = String("01011");                            //R.07
+    bitString = String("11111");                            //R.07 *
   }
-  else if (CellName == "82464856")                          //R.08
+  else if (CellName == "82464856")                          //R.08 *
   {
-    bitString = String("01010");                            //R.08
+    bitString = String("11110");                            //R.08 *
   }
-  else if (CellName == "82464857")                          //R.09
+  else if (CellName == "82464857")                          //R.09 *
   {
-    bitString = String("00010");                            //R.09
+    bitString = String("11010");                            //R.09 *
   }
-  else if (CellName == "82464948")                          //R.10
+  else if (CellName == "82464948")                          //R.10 *
   {
-    bitString = String("00011");                            //R.10
+    bitString = String("11011");                            //R.10 *
   }
-  else if (CellName == "82464949")                          //R.11
+  else if (CellName == "82464949")                          //R.11 *
   {
-    bitString = String("00001");                            //R.11
+    bitString = String("11001");                            //R.11 *
   }
-  else if (CellName == "82464950")                          //R.12
+  else if (CellName == "82464950")                          //R.12 *
   {
-    bitString = String("00000");                            //R.12
+    bitString = String("11000");                            //R.12 *
   }
-  else if (CellName == "76464849")                          //L.01
+  else if (CellName == "76464849")                          //L.01 *
   {
-    bitString = String("10000");                            //L.01
+    bitString = String("00010");                            //L.01 *
   }
-  else if (CellName == "76464850")                          //L.02
+  else if (CellName == "76464850")                          //L.02 *
   {
-    bitString = String("10001");                            //L.02
+    bitString = String("00011");                            //L.02 *
   }
-  else if (CellName == "76464851")                          //L.03
+  else if (CellName == "76464851")                          //L.03 *
   {
-    bitString = String("10011");                            //L.03
+    bitString = String("00001");                            //L.03 *
   }
-  else if (CellName == "76464852")                          //L.04
+  else if (CellName == "76464852")                          //L.04 *
   {
-    bitString = String("10010");                            //L.04
+    bitString = String("00000");                            //L.04 *
   }
-  else if (CellName == "76464853")                          //L.05
+  else if (CellName == "76464853")                          //L.05 *
   {
-    bitString = String("11110");                            //L.05
+    bitString = String("10100");                            //L.05 *
   }
-  else if (CellName == "76464854")                          //L.06
+  else if (CellName == "76464854")                          //L.06 *
   {
-    bitString = String("11111");                            //L.06
+    bitString = String("10101");                            //L.06 *
   }
-  else if (CellName == "76464855")                          //L.07
+  else if (CellName == "76464855")                          //L.07 *
   {
-    bitString = String("11101");                            //L.07
+    bitString = String("10111");                            //L.07 *
   }
-  else if (CellName == "76464856")                          //L.08
+  else if (CellName == "76464856")                          //L.08 *
   {
-    bitString = String("11100");                            //L.08
+    bitString = String("10110");                            //L.08 *
   }
-  else if (CellName == "76464857")                          //L.09
+  else if (CellName == "76464857")                          //L.09 *
   {
-    bitString = String("11000");                            //L.09
+    bitString = String("10010");                            //L.09 *
   }
-  else if (CellName == "76464948")                          //L.10
+  else if (CellName == "76464948")                          //L.10 *
   {
-    bitString = String("11001");                            //L.10
+    bitString = String("10011");                            //L.10 *
   }
-  else if (CellName == "76464949")                          //L.11
+  else if (CellName == "76464949")                          //L.11 *
   {
-    bitString = String("11011");                            //L.11
+    bitString = String("10001");                            //L.11 *
   }
-  else if (CellName == "76464950")                          //L.12
+  else if (CellName == "76464950")                          //L.12 *
   {
-    bitString = String("11010");                            //L.12
+    bitString = String("10000");                            //L.12 *
   }
   else if (HotCold == "72")                                   // heating
   {
@@ -314,7 +315,7 @@ String SelectedCellName(String CellName)
       digitalWrite(HnCPin, LOW);                               // Bring the pin low to set it for heating
       delay(10);
       analogWrite(PWMPin, PWMSignal);                          //Write the power at wich you want to heat up
-      //Serial.println(PWMSignal);
+      Serial.println(PWMSignal);
     }
   }
   else if (HotCold == "67")                                               // Cooling
@@ -327,7 +328,7 @@ String SelectedCellName(String CellName)
       digitalWrite(HnCPin, HIGH);                               // Bring the pin high to set it for cooling
       delay(10);
       analogWrite(PWMPin, PWMSignal);                          //Write the power at wich you want to cool down
-      //Serial.println(PWMSignal);
+      Serial.println(PWMSignal);
     }
   }
   else
