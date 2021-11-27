6491c5e1d2b54fa29d3a9ce1afeb08c8bd91ab34
minusmagis <minu_10@hotmail.com>
2021-11-27 15:22:10 +0100

    small updates

--- Arduino Files/Pika_demultiplexer_4.0/Definitions.h
+++ Arduino Files/Pika_demultiplexer_4.0/Definitions.h
@@ -2,11 +2,22 @@
  * This file contains all the definitions for constants used throughout all the subroutines
  */
 
-// We define the Delay for the Relay actuation, so that the relays have time to change from one to the next (in milliseconds)
-#define Relay_Delay 1000
+// We define the time we want the test run to stay within each LED (in milliseconds)
+#define Test_Delay_1 200
 
 // We define the time we want the test run to stay within each LED (in milliseconds)
-#define Test_Delay 200
+#define Test_Delay_2 50
 
 // We define (or not) a debugging variable to force some printouts for debugging the software
-// #define Debugging 0
+//#define Debugging 0
+
+// We define which board version are we using
+#define Board_Version 3
+
+// We define the Delay for the Relay actuation, so that the relays have time to change from one to the next (in milliseconds)
+// In case we are debugging we set the delay for a much longer time to properly diagnose the board
+#if defined Debugging
+  #define Relay_Delay 200
+#else
+  #define Relay_Delay 50
+#endif
--- Arduino Files/Pika_demultiplexer_4.0/Relay_Actuation.ino
+++ Arduino Files/Pika_demultiplexer_4.0/Relay_Actuation.ino
@@ -4,9 +4,14 @@
 #include "Definitions.h"
 
 // We also define a constant 2D array that defines which arduino pins are connected to which Set and Reset pins on the PCB
+#if defined Board_Version && Board_Version == 3
+const int Set_Pins[][4] = {{3}, {4}, {5}, {6}, {7}};
+const int Reset_Pins[][4] = {{8}, {9}, {10}, {11}, {12}};
+#else
 // In this case the Set pin 1 is connected to pin D3 on the arduino, Set pin 2 to pin D18 (A4), Set pin 3 to pin D4, Set pin 4 to pins 15 and 16 (A0,A1) and Set pin 5 to pins D10-D13
 const int Set_Pins[][4] = {{3}, {18}, {4}, {15, 16}, {10, 11, 12, 13}};
 const int Reset_Pins[][4] = {{2}, {19}, {5}, {17, 18}, {6, 7, 8, 9}};
+#endif
 
 // We import the extern char array that stores the current binary label used on the label translator script
 extern char Binary_Label[];
@@ -18,44 +23,38 @@ int Reset_Pin_To_Actuate = 0;
 void Relay_Actuate()
 {
   for (int j = 0; j < 5; j++) {                             // We run through all the values of the Binary label (5 values)
-    
+
     int Pin_state = Binary_Label[j] - 48;                   // We convert the current char to an integer by substracting the '0' char value (48)
-    
+
     for (int k = 0; k < 4; k++) {                           // We loop through all the pins stored within the array (maximum of 4)
-      
+
       Set_Pin_To_Actuate =  Set_Pins[j][k];                 // We extract the Set pin number from the Set_Pins array as well as the Reset pin number from the Reset_Pins array
       Reset_Pin_To_Actuate =  Reset_Pins[j][k];
 
       if (Set_Pin_To_Actuate != 0) {                        // We only actuate the pin if the pin number is not 0
-        #ifdef Debugging
-        Print_Pin_Status(Reset_Pin_To_Actuate, Pin_state);  // If we are debugging we print the state of each pin
-        #endif
-        digitalWrite(Set_Pin_To_Actuate, Pin_state);        // We write the state of the current Binary label number to the selected Set pin
+        Digital_Write_Pin(Set_Pin_To_Actuate, Pin_state);        // We write the state of the current Binary label number to the selected Set pin
       }
-      
+
       if (Reset_Pin_To_Actuate != 0) {                      // We only actuate the pin if the pin number is not 0
-        #ifdef Debugging
-        Print_Pin_Status(Reset_Pin_To_Actuate, Pin_state);      // If we are debugging we print the state of each pin
-        #endif
-        digitalWrite(Reset_Pin_To_Actuate, invert(Pin_state));  // We write the inverse state of the current Binary label number to the selected Reset pin
+        Digital_Write_Pin(Reset_Pin_To_Actuate, invert(Pin_state));  // We write the inverse state of the current Binary label number to the selected Reset pin
       }
     }
-    
+
     delay(Relay_Delay);                                     // We give some time to the relays to get into position
     Reset_All_Pin_States();                                 // We reset all the pins to low to save power
-    
+
   }
 }
 
 // This function sets all the digital pins to LOW
 void Reset_All_Pin_States() {
-  
+
   for (int pin = 2; pin <= 19; pin++)
   {
     digitalWrite(pin, LOW);
   }
-  
-delay(Relay_Delay);         // We wait a bit so the relays gan get into position
+
+  delay(Relay_Delay);         // We wait a bit so the relays gan get into position
 }
 
 
@@ -72,10 +71,29 @@ uint8_t invert(uint8_t value)
 
 
 // This function prints the current pin number and its status for debugging purposes
-void Print_Pin_Status(int Pin_Number, int Pin_State)
+void Print_Pin_Status(int Pin_Number, boolean Pin_State)
 {
   Serial.print("Pin number: ");
   Serial.print(Pin_Number);
   Serial.print(" is ");
   Serial.println(Pin_State);
+}
+
+// This function wites high or low on the specified pin depending on the Pin_State Input
+void Digital_Write_Pin(int Pin_Number, int Pin_State)
+{
+  boolean Actual_Write = false;
+  if (Pin_State == 1)
+  {
+    digitalWrite(Pin_Number, HIGH);
+    Actual_Write = true;
+  }
+  else
+  {
+    digitalWrite(Pin_Number, LOW);
+    Actual_Write = false;
+  }
+#ifdef Debugging
+  Print_Pin_Status(Pin_Number, Actual_Write);      // If we are debugging we print the state of each pin
+#endif
 }
